f016e23b90270b41f6fab9c438b294b22759bb9d
killsazer <vadym2099@gmail.com>
2025-06-01 19:17:17 +0300

    Cursore invisible now.

--- Kursach/GameRunner.cs
+++ Kursach/GameRunner.cs
@@ -9,19 +9,17 @@ class Runner
     static void Main()
     {
         Console.OutputEncoding = Encoding.UTF8;
-        // var menu = new StartMenu();
-        // bool continueCondition = menu.MenuGenerator();
+        Console.CursorVisible = false;
+        var menu = new StartMenu();
+        bool continueCondition = menu.MenuGenerator();
 
-        // while (continueCondition == true)
-        // {
-        //     var game = new Game();
-        //     continueCondition = game.StartGame();
-        // }
-        // Console.Clear();
-        // Console.WriteLine("\n Thanks for playing! Press any key to exit...");
-        // Console.ReadKey(true);
-
-        var game = new Game();
-        game.StartGame();
+        while (continueCondition == true)
+        {
+            var game = new Game();
+            continueCondition = game.StartGame();
+        }
+        Console.Clear();
+        Console.WriteLine("\n Thanks for playing! Press any key to exit...");
+        Console.ReadKey(true);
     }
 }
